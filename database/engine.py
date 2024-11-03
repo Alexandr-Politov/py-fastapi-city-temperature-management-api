eff9383c19503eace1710d79c648d243aab89119
Alexandr Politov <apolitov83@gmail.com>
2024-11-03 13:21:21 +0200

    engine.py for database created

---
+++ database/engine.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./city_temperature.db"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+BaseSQLAlchemyModel = declarative_base()
+    
